73bc530534b46034ecf98e4a4bb09248d9f7112e
ShimpyPurple <aidanparshall@gmail.com>
2023-04-27 14:36:58 -0700

    Start gripper constructors

--- src/EE335_Gripper.cpp
+++ src/EE335_Gripper.cpp
@@ -1,7 +1,14 @@
 #include "EE335_Gripper.h"
 
-Gripper::Gripper( uint8_t pin ):
-    pin( pin )
+Gripper::Gripper( uint8_t pin , ServoManager servoManager ):
+    pin( pin ) ,
+    servoManager( servoManager )
 {
     
+}
+
+Gripper::Gripper( uint8_t pin , uint8_t timer ):
+    pin( pin ) ,
+{
+    servoManager = new ServoManager( timer );
 }
--- src/EE335_Gripper.h
+++ src/EE335_Gripper.h
@@ -3,6 +3,7 @@
 
 #include "Arduino.h"
 #include "CustomTimers.h"
+#include "CustomServos.h"
 
 #if !defined( __AVR_ATmega328P__ ) && !defined( __AVR_ATmega2560__ )
 #warning "EE335_Gripper is only tested for ATmega328P and ATmega2560"
@@ -10,12 +11,12 @@
 
 class Gripper {
     public:
-        Gripper( uint8_t pin );
-        // Gripper( uint8_t pin , uint8_t timer );
-        
+        Gripper( uint8_t pin , ServoManager servoManager );
+        Gripper( uint8_t pin , uint8_t timer );
     
     private:
         uint8_t pin;
+        ServoManager *servoManager;
         
     
 };
