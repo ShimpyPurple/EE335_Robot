538b5731961312876a78c365940de9fbb93ca7ae
ShimpyPurple <aidanparshall@gmail.com>
2023-04-27 15:03:47 -0700

    Simple open close methods

--- src/EE335_Gripper.cpp
+++ src/EE335_Gripper.cpp
@@ -1,14 +1,38 @@
 #include "EE335_Gripper.h"
 
-Gripper::Gripper( uint8_t pin , ServoManager servoManager ):
+Gripper::Gripper( uint8_t pin , ServoManager *servoManager ):
     pin( pin ) ,
     servoManager( servoManager )
-{
-    
-}
+{}
 
 Gripper::Gripper( uint8_t pin , uint8_t timer ):
-    pin( pin ) ,
+    pin( pin )
 {
     servoManager = new ServoManager( timer );
+}
+
+Gripper::Gripper( uint8_t pin , BaseTimer16 *timer16 ):
+    pin( pin )
+{
+    servoManager = new ServoManager( timer16 );
+}
+
+Gripper::Gripper( uint8_t pin , BaseTimer8Async *timer8 ):
+    pin( pin )
+{
+    servoManager = new ServoManager( timer8 );
+}
+
+Gripper::Gripper( uint8_t pin , GenericTimer *timer ):
+    pin( pin )
+{
+    servoManager = new ServoManager( timer );
+}
+
+void Gripper::open() {
+    servoManager->write( pin , OPEN_PERCENT );
+}
+
+void Gripper::close() {
+    servoManager->write( pin , CLOSE_PERCENT );
 }
--- src/EE335_Gripper.h
+++ src/EE335_Gripper.h
@@ -9,15 +9,23 @@
 #warning "EE335_Gripper is only tested for ATmega328P and ATmega2560"
 #endif
 
+#define CLOSE_PERCENT 25
+#define OPEN_PERCENT 75
+
 class Gripper {
     public:
-        Gripper( uint8_t pin , ServoManager servoManager );
+        Gripper( uint8_t pin , ServoManager *servoManager );
         Gripper( uint8_t pin , uint8_t timer );
+        Gripper( uint8_t pin , BaseTimer16 *timer16 );
+        Gripper( uint8_t pin , BaseTimer8Async *timer8 );
+        Gripper( uint8_t pin , GenericTimer *timer );
+        
+        void open();
+        void close();
     
     private:
         uint8_t pin;
         ServoManager *servoManager;
-        
     
 };
 
