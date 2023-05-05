67eb3e7962cd065e2e9e96aced6c02d05bc111b4
ShimpyPurple <aidanparshall@gmail.com>
2023-05-04 17:49:58 -0700

    Prototype Lineread class

---
+++ src/EE335_Lineread.h
@@ -0,0 +1,24 @@
+#ifndef EE335_Lineread_h
+#define EE335_Lineread_h
+
+#include "Arduino.h"
+#include "CustomInterrupts.h"
+#include "Constants/Constants.h"
+
+#if !defined( __AVR_ATmega328P__ ) && !defined( __AVR_ATmega2560__ )
+#warning "EE335_Lineread is only tested for ATmega328P and ATmega2560"
+#endif
+
+class Lineread {
+    public:
+        Lineread( uint8_t pin );
+        void begin();
+    
+    private:
+        uint8_t pin;
+        unsigned long previnst;
+        static void lineread_ISR( void *object );
+        
+};
+
+#endif
