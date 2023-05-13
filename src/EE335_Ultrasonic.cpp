534ff95f2ae2659f3b4815573fba6aa8f2c73d8e
ShimpyPurple <aidanparshall@gmail.com>
2023-05-13 16:08:05 -0700

    Begin Ultrasonic Class

--- src/EE335_Ultrasonic.cpp
+++ src/EE335_Ultrasonic.cpp
@@ -1,2 +1,23 @@
 #include "EE335_Ultrasonic.h"
 
+Ultrasonic::Ultrasonic( uint8_t trigPin , uint8_t echoPin , ServoManager *servoManager , float mach=34300 ):
+    trigPin( trigPin ) ,
+    echoPin( echoPin ) ,
+    servoManager( servoManager ) ,
+    mach( mach )
+{}
+
+void Ultrasonic::begin() {
+    pinMode( trigPin , OUTPUT );
+    pinMode( echoPin , INPUT );
+    
+    factor = mach / ( 2 * timer->getTickRate() );
+}
+
+float Ultrasonic::getDistance() {
+    
+}
+
+static void Ultrasonic::echoReceived( void *object , uint8_t edgeType ) {
+    Ultrasonic *ultrasonic = ( Ultrasonic* )( object );
+}
--- src/EE335_Ultrasonic.h
+++ src/EE335_Ultrasonic.h
@@ -2,12 +2,26 @@
 #define EE335_Ultrasonic_h
 
 #include "Arduino.h"
+#include "CustomServos.h"
 #include "Constants/Constants.h"
 
 #if !defined( __AVR_ATmega328P__ ) && !defined( __AVR_ATmega2560__ )
 #warning "EE335_Ultrasonic is only tested for ATmega328P and ATmega2560"
 #endif
 
-
+class Ultrasonic {
+    public:
+        Ultrasonic( uint8_t trigPin , uint8_t echoPin , ServoManager *servoManager , float mach );
+        void begin();
+        float getDistance();
+    
+    private:
+        uint8_t trigPin;
+        uint8_t echoPin;
+        ServoManager *servoManager;
+        float mach;
+        float factor;
+    
+};
 
 #endif
