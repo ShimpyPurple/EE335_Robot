9c122a87d933ac115f5635b66bad350708562148
ShimpyPurple <aidanparshall@gmail.com>
2023-05-26 18:30:38 -0700

    Add support for CustomMotorShield

--- src/EE335_Motor.cpp
+++ src/EE335_Motor.cpp
@@ -1,6 +1,21 @@
 #include "EE335_Motor.h"
 
+Motor::Motor( MotorShield *motorShield , uint8_t motorNumber ):
+    driverType( TYPE_CUSTOM_MOTOR_SHIELD ) ,
+    motorShield( motorShield ) ,
+    motorNumber( motorNumber ) ,
+    pid( new PID(1,3) ) ,
+    cruiseEnabled( false ) ,
+    currentDirection( RELEASE ) ,
+    directionToSet( 0 ) ,
+    updateDirectionFlag( false ) ,
+    currentPWM( 0 ) ,
+    pwmToSet( 0 ) ,
+    updatePWMFlag( false )
+{}
+
 Motor::Motor( Adafruit_DCMotor *adafruitDCMotor ):
+    driverType( TYPE_ADAFRUIT_DC_MOTOR ) ,
     adafruitDCMotor( adafruitDCMotor ) ,
     pid( new PID(1,3) ) ,
     cruiseEnabled( false ) ,
@@ -13,7 +28,7 @@ Motor::Motor( Adafruit_DCMotor *adafruitDCMotor ):
 {}
 
 Motor::Motor( uint8_t pwmPin , uint8_t dirPin1 , uint8_t dirPin2 ):
-    adafruitDCMotor( nullptr ) ,
+    driverType( TYPE_H_BRIDGE ) ,
     pwmPin( pwmPin ) ,
     dirPin1( dirPin1 ) ,
     dirPin2( dirPin2 ) ,
@@ -28,35 +43,36 @@ Motor::Motor( uint8_t pwmPin , uint8_t dirPin1 , uint8_t dirPin2 ):
 {}
 
 void Motor::begin() {
-    if ( adafruitDCMotor == nullptr ) {
+    if ( driverType == TYPE_H_BRIDGE ) {
         pinMode( pwmPin  , OUTPUT );
         pinMode( dirPin1 , OUTPUT );
         pinMode( dirPin2 , OUTPUT );
     }
+    
     setDirection( RELEASE );
     setPWM( 0 );
 }
 
 void Motor::setDirection( uint8_t direction ) {
-    currentDirection = direction;
-    if ( adafruitDCMotor == nullptr ) {
-        switch ( direction ) {
-            case FORWARD:
-                digitalWrite( dirPin2 , LOW );
-                digitalWrite( dirPin1 , HIGH );
-                break;
-            case BACKWARD:
-                digitalWrite( dirPin1 , LOW );
-                digitalWrite( dirPin2 , HIGH );
-                break;
-            case RELEASE:
-                digitalWrite( dirPin1 , LOW );
-                digitalWrite( dirPin2 , LOW );
-                break;
-        }
-    } else {
-        adafruitDCMotor->run( direction );
+    switch ( driverType ) {
+        case TYPE_CUSTOM_MOTOR_SHIELD:
+            motorShield->setMotorDirection( motorNumber , direction );
+            break;
+        case TYPE_ADAFRUIT_DC_MOTOR:
+            adafruitDCMotor->run( direction == BRAKE ? RELEASE : direction );
+            break;
+        case TYPE_H_BRIDGE:
+            switch ( direction ) {
+                case FORWARD:  digitalWrite( dirPin2 , LOW  ); digitalWrite( dirPin1 , HIGH ); break;
+                case BACKWARD: digitalWrite( dirPin1 , LOW  ); digitalWrite( dirPin2 , HIGH ); break;
+                case BRAKE:    digitalWrite( dirPin1 , HIGH ); digitalWrite( dirPin2 , HIGH ); break;
+                case RELEASE:  digitalWrite( dirPin1 , LOW  ); digitalWrite( dirPin2 , LOW  ); break;
+            }
+            break;
+        default: return;
     }
+    
+    currentDirection = direction;
 }
 
 void Motor::requestDirection( uint8_t direction ) {
@@ -66,20 +82,45 @@ void Motor::requestDirection( uint8_t direction ) {
     }
 }
 
-void Motor::setPWM( uint8_t val ) {
-    currentPWM = val;
-    if ( adafruitDCMotor == nullptr ) {
-        analogWrite( pwmPin , val );
-    } else {
-        adafruitDCMotor->setSpeed( val );
+void Motor::setPWM( uint16_t val ) {
+    switch ( driverType ) {
+        case TYPE_CUSTOM_MOTOR_SHIELD:
+            motorShield->setMotorPWM( motorNumber , val );
+            break;
+        case TYPE_ADAFRUIT_DC_MOTOR:
+            if ( val == 0 ) {
+                adafruitDCMotor->fullOff();
+            } else if ( val >= 0x1000 ) {
+                adafruitDCMotor->fullOn();
+            } else {
+                adafruitDCMotor->setSpeedFine( val );
+            }
+            break;
+        case TYPE_H_BRIDGE:
+            analogWrite( pwmPin , val/16 );
+        default: return;
     }
+    
+    currentPWM = val;
 }
 
-void Motor::requestPWM( uint8_t val ) {
+void Motor::requestPWM( uint16_t val ) {
     if ( val != currentPWM ) {
         pwmToSet = val;
         updatePWMFlag = true;
     }
+}
+
+void Motor::setPercent( float percent ) {
+    if ( percent > 100 ) percent = 100;
+    if ( percent <   0 ) percent =   0;
+    setPWM( percent/100 * 0x1000 );
+}
+
+void Motor::requestPercent( float percent ) {
+    if ( percent > 100 ) percent = 100;
+    if ( percent <   0 ) percent =   0;
+    requestPWM( percent/100 * 0x1000 );
 }
 
 void Motor::update() {
@@ -98,6 +139,7 @@ void Motor::attachEncoder( Encoder *encoder ) {
 }
 
 void Motor::enableCruise() {
+    if ( cruiseEnabled ) return;
     cruiseEnabled = true;
     cruiseID = runAfter(
         50 ,
--- src/EE335_Motor.h
+++ src/EE335_Motor.h
@@ -7,20 +7,28 @@
 #include "Constants/Constants.h"
 #include "PID/EE335_PID.h"
 #include "CustomInterrupts.h"
+#include "CustomMotorShield.h"
 
 #if !defined( __AVR_ATmega328P__ ) && !defined( __AVR_ATmega2560__ )
 #warning "EE335_Motor is only tested for ATmega328P and ATmega2560"
 #endif
 
+#define TYPE_CUSTOM_MOTOR_SHIELD 0
+#define TYPE_ADAFRUIT_DC_MOTOR 1
+#define TYPE_H_BRIDGE 3
+
 class Motor {
     public:
+        Motor( MotorShield *motorShield , uint8_t motorNumber );
         Motor( Adafruit_DCMotor *adafruitDCMotor );
         Motor( uint8_t pwmPin , uint8_t dirPin1 , uint8_t dirPin2 );
         void begin();
         void setDirection( uint8_t direction );
         void requestDirection( uint8_t direction );
-        void setPWM( uint8_t val );
-        void requestPWM( uint8_t val );
+        void setPWM( uint16_t val );
+        void requestPWM( uint16_t val );
+        void setPercent( float percent );
+        void requestPercent( float percent );
         void update();
         void attachEncoder( Encoder *encoder );
         void enableCruise();
@@ -28,18 +36,21 @@ class Motor {
         void stopCruise();
         
     private:
-        Adafruit_DCMotor *adafruitDCMotor;
-        uint8_t pwmPin;
-        uint8_t dirPin1;
-        uint8_t dirPin2;
+        const uint8_t driverType;
+        const MotorShield *motorShield;
+        const uint8_t motorNumber;
+        const Adafruit_DCMotor *adafruitDCMotor;
+        const uint8_t pwmPin;
+        const uint8_t dirPin1;
+        const uint8_t dirPin2;
         uint8_t currentDirection;
         uint8_t directionToSet;
         bool updateDirectionFlag;
         uint8_t currentPWM;
         uint8_t pwmToSet;
         bool updatePWMFlag;
-        Encoder *encoder;
-        PID *pid;
+        const Encoder *encoder;
+        const PID *pid;
         bool cruiseEnabled;
         uint8_t cruiseID;
     
