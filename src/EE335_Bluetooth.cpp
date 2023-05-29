74212f1958a681cbc220abc6bb60a382ed3b05b3
ShimpyPurple <aidanparshall@gmail.com>
2023-05-29 13:32:47 -0700

    Begin working on bluetooth send

--- src/EE335_Bluetooth.cpp
+++ src/EE335_Bluetooth.cpp
@@ -1,6 +1,12 @@
 #include "EE335_Bluetooth.h"
 
-Bluetooth::Bluetooth( uint8_t serialPort ) {
+void idle() __attribute__( (weak) );
+
+Bluetooth::Bluetooth( uint8_t serialPort , void (**onRx)(Bluetooth*) , uint16_t sendCooldown ):
+    sendCooldown( sendCooldown ) ,
+    lastSendTime( 0 ) ,
+    instructionReceived( false )
+{
     switch ( serialPort ) {
         case SERIAL_0: btSerial = &Serial;  break;
 #if defined( __AVR_ATmega2560__ )
@@ -10,14 +16,23 @@ Bluetooth::Bluetooth( uint8_t serialPort ) {
 #endif
         default: btSerial = &Serial;  break;
     }
+    
+    if ( onRx != nullptr ) {
+        *onRx = []( Bluetooth *object ) {
+            Bluetooth *bluetooth = ( Bluetooth* )( object );
+            if ( bluetooth->getInstruction() ) {
+                bluetooth->instructionReceived = true;
+            }
+        };
+    }
 }
 
 void Bluetooth::begin() {
     btSerial->begin( 9600 );
 }
 
-void Bluetooth::getInstruction() {
-    waitForSerial();
+bool Bluetooth::getInstruction() {
+    if ( !btSerial->available() ) return false;
     
     uint8_t header = btSerial->read();
     
@@ -25,101 +40,113 @@ void Bluetooth::getInstruction() {
         waitForSerial();
         uint8_t inByte = btSerial->read();
         if ( inByte < R1_START ) {
-            ControllerState.rightJoystickRadius = 0;
-            ControllerState.rightJoystickAngle  = 0;
+            controllerState.rightJoystickRadius = 0;
+            controllerState.rightJoystickAngle  = 0;
         } else if ( inByte < R2_START ) {
-            ControllerState.rightJoystickRadius = 1;
-            ControllerState.rightJoystickAngle  = (float)( inByte - R1_START ) / ( R2_START - R1_START );
+            controllerState.rightJoystickRadius = 1;
+            controllerState.rightJoystickAngle  = (float)( inByte - R1_START ) / ( R2_START - R1_START );
         } else if ( inByte < R3_START ) {
-            ControllerState.rightJoystickRadius = 2;
-            ControllerState.rightJoystickAngle  = (float)( inByte - R2_START ) / ( R3_START - R2_START );
+            controllerState.rightJoystickRadius = 2;
+            controllerState.rightJoystickAngle  = (float)( inByte - R2_START ) / ( R3_START - R2_START );
         } else if ( inByte < R4_START ) {
-            ControllerState.rightJoystickRadius = 3;
-            ControllerState.rightJoystickAngle  = (float)( inByte - R3_START ) / ( R4_START - R3_START );
+            controllerState.rightJoystickRadius = 3;
+            controllerState.rightJoystickAngle  = (float)( inByte - R3_START ) / ( R4_START - R3_START );
         } else if ( inByte < R5_START ) {
-            ControllerState.rightJoystickRadius = 4;
-            ControllerState.rightJoystickAngle  = (float)( inByte - R4_START ) / ( R5_START - R4_START );
+            controllerState.rightJoystickRadius = 4;
+            controllerState.rightJoystickAngle  = (float)( inByte - R4_START ) / ( R5_START - R4_START );
         } else if ( inByte < R6_START ) {
-            ControllerState.rightJoystickRadius = 5;
-            ControllerState.rightJoystickAngle  = (float)( inByte - R5_START ) / ( R6_START - R5_START );
+            controllerState.rightJoystickRadius = 5;
+            controllerState.rightJoystickAngle  = (float)( inByte - R5_START ) / ( R6_START - R5_START );
         } else if ( inByte < R7_START ) {
-            ControllerState.rightJoystickRadius = 6;
-            ControllerState.rightJoystickAngle  = (float)( inByte - R6_START ) / ( R7_START - R6_START );
+            controllerState.rightJoystickRadius = 6;
+            controllerState.rightJoystickAngle  = (float)( inByte - R6_START ) / ( R7_START - R6_START );
         } else {
-            ControllerState.rightJoystickRadius = 7;
-            ControllerState.rightJoystickAngle  = (float)( inByte - R7_START ) / ( 0x100 - R7_START );
+            controllerState.rightJoystickRadius = 7;
+            controllerState.rightJoystickAngle  = (float)( inByte - R7_START ) / ( 0x100 - R7_START );
         }
-        if ( ControllerState.rightJoystickAngle > 0.5 ) {
-            ControllerState.rightJoystickAngle -= 1;
+        if ( controllerState.rightJoystickAngle > 0.5 ) {
+            controllerState.rightJoystickAngle -= 1;
         }
-        ControllerState.rightJoystickAngle *= 2 * M_PI;
+        controllerState.rightJoystickAngle *= 2 * M_PI;
     }
     
     if ( header & (1<<L_JOYSTICK) ) {
         waitForSerial();
         uint8_t inByte = btSerial->read();
         if ( inByte < R1_START ) {
-            ControllerState.leftJoystickRadius = 0;
-            ControllerState.leftJoystickAngle  = 0;
+            controllerState.leftJoystickRadius = 0;
+            controllerState.leftJoystickAngle  = 0;
         } else if ( inByte < R2_START ) {
-            ControllerState.leftJoystickRadius = 1;
-            ControllerState.leftJoystickAngle  = (float)( inByte - R1_START ) / ( R2_START - R1_START );
+            controllerState.leftJoystickRadius = 1;
+            controllerState.leftJoystickAngle  = (float)( inByte - R1_START ) / ( R2_START - R1_START );
         } else if ( inByte < R3_START ) {
-            ControllerState.leftJoystickRadius = 2;
-            ControllerState.leftJoystickAngle  = (float)( inByte - R2_START ) / ( R3_START - R2_START );
+            controllerState.leftJoystickRadius = 2;
+            controllerState.leftJoystickAngle  = (float)( inByte - R2_START ) / ( R3_START - R2_START );
         } else if ( inByte < R4_START ) {
-            ControllerState.leftJoystickRadius = 3;
-            ControllerState.leftJoystickAngle  = (float)( inByte - R3_START ) / ( R4_START - R3_START );
+            controllerState.leftJoystickRadius = 3;
+            controllerState.leftJoystickAngle  = (float)( inByte - R3_START ) / ( R4_START - R3_START );
         } else if ( inByte < R5_START ) {
-            ControllerState.leftJoystickRadius = 4;
-            ControllerState.leftJoystickAngle  = (float)( inByte - R4_START ) / ( R5_START - R4_START );
+            controllerState.leftJoystickRadius = 4;
+            controllerState.leftJoystickAngle  = (float)( inByte - R4_START ) / ( R5_START - R4_START );
         } else if ( inByte < R6_START ) {
-            ControllerState.leftJoystickRadius = 5;
-            ControllerState.leftJoystickAngle  = (float)( inByte - R5_START ) / ( R6_START - R5_START );
+            controllerState.leftJoystickRadius = 5;
+            controllerState.leftJoystickAngle  = (float)( inByte - R5_START ) / ( R6_START - R5_START );
         } else if ( inByte < R7_START ) {
-            ControllerState.leftJoystickRadius = 6;
-            ControllerState.leftJoystickAngle  = (float)( inByte - R6_START ) / ( R7_START - R6_START );
+            controllerState.leftJoystickRadius = 6;
+            controllerState.leftJoystickAngle  = (float)( inByte - R6_START ) / ( R7_START - R6_START );
         } else {
-            ControllerState.leftJoystickRadius = 7;
-            ControllerState.leftJoystickAngle  = (float)( inByte - R7_START ) / ( 0x100 - R7_START );
+            controllerState.leftJoystickRadius = 7;
+            controllerState.leftJoystickAngle  = (float)( inByte - R7_START ) / ( 0x100 - R7_START );
         }
-        if ( ControllerState.leftJoystickAngle > 0.5 ) {
-            ControllerState.leftJoystickAngle -= 1;
+        if ( controllerState.leftJoystickAngle > 0.5 ) {
+            controllerState.leftJoystickAngle -= 1;
         }
-        ControllerState.leftJoystickAngle *= 2 * M_PI;
+        controllerState.leftJoystickAngle *= 2 * M_PI;
     }
     
     if ( header & (1<<TRIGGERS) ) {
         waitForSerial();
         uint8_t inByte = btSerial->read();
-        ControllerState.rightTrigger = inByte & 0x0F;
-        ControllerState.leftTrigger  = inByte >> 4;
+        controllerState.rightTrigger = inByte & 0x0F;
+        controllerState.leftTrigger  = inByte >> 4;
     }
     
     if ( header & (1<<DPAD) ) {
         waitForSerial();
         uint8_t inByte = btSerial->read();
-        ControllerState.dPadUp    = ( inByte & (1<<DPAD_UP)    );
-        ControllerState.dPadDown  = ( inByte & (1<<DPAD_DOWN)  );
-        ControllerState.dPadRight = ( inByte & (1<<DPAD_RIGHT) );
-        ControllerState.dPadLeft  = ( inByte & (1<<DPAD_LEFT)  );
+        controllerState.dPadUp    = ( inByte & (1<<DPAD_UP)    );
+        controllerState.dPadDown  = ( inByte & (1<<DPAD_DOWN)  );
+        controllerState.dPadRight = ( inByte & (1<<DPAD_RIGHT) );
+        controllerState.dPadLeft  = ( inByte & (1<<DPAD_LEFT)  );
     }
     
     if ( header & (1<<BUTTONS) ) {
         waitForSerial();
         uint8_t inByte = btSerial->read();
-        ControllerState.buttonA = ( inByte & (1<<BUTTON_A) );
-        ControllerState.buttonB = ( inByte & (1<<BUTTON_B) );
-        ControllerState.buttonX = ( inByte & (1<<BUTTON_X) );
-        ControllerState.buttonY = ( inByte & (1<<BUTTON_Y) );
-        ControllerState.rightBumper = ( inByte & (1<<R_BUMPER) );
-        ControllerState.leftBumper  = ( inByte & (1<<L_BUMPER) );
-        ControllerState.select = ( inByte & (1<<SELECT) );
-        ControllerState.start  = ( inByte & (1<<START)  );
+        controllerState.buttonA = ( inByte & (1<<BUTTON_A) );
+        controllerState.buttonB = ( inByte & (1<<BUTTON_B) );
+        controllerState.buttonX = ( inByte & (1<<BUTTON_X) );
+        controllerState.buttonY = ( inByte & (1<<BUTTON_Y) );
+        controllerState.rightBumper = ( inByte & (1<<R_BUMPER) );
+        controllerState.leftBumper  = ( inByte & (1<<L_BUMPER) );
+        controllerState.select = ( inByte & (1<<SELECT) );
+        controllerState.start  = ( inByte & (1<<START)  );
     }
     
+    return true;
 }
 
 void Bluetooth::waitForSerial() {
-    while ( !btSerial->available() );
+    while ( !btSerial->available() ) {
+        idle();
+    }
+}
+
+void Bluetooth::sendState() {
+    uint32_t ms = millis();
+    if ( ms - lastSendTime < sendCooldown ) return;
+    
+    
+    
+    
 }
--- src/EE335_Bluetooth.h
+++ src/EE335_Bluetooth.h
@@ -40,11 +40,14 @@
 #define SELECT 6
 #define START  7
 
+#define ALL_HEADINGS 0xFF
+
 class Bluetooth {
     public:
-        Bluetooth( uint8_t serialPort );
+        Bluetooth( uint8_t serialPort , void (**onRx)(Bluetooth*)=nullptr , uint16_t sendCooldown=10 );
         void begin();
-        void getInstruction();
+        bool getInstruction();
+        bool instructionReceived;
         struct {
             uint8_t rightJoystickRadius = 0;
             uint8_t leftJoystickRadius  = 0;
@@ -64,11 +67,49 @@ class Bluetooth {
             bool leftBumper  = false;
             bool select = false;
             bool start  = false;
-        } ControllerState;
+        } controllerState;
+        struct {
+            uint8_t speed = 0;
+            uint8_t cruiseSpeed = 0;
+            bool cruiseControl = false;
+            bool lineFollowing = false;
+            void setSpeed( float speed , float minSpeed , float maxSpeed ) {
+                if ( speed < minSpeed ) speed = minSpeed;
+                if ( speed > maxSpeed ) speed = maxSpeed;
+                this->speed = ( speed - minSpeed ) / ( maxSpeed - minSpeed );
+            }
+            void setCruiseSpeed( float cruiseSpeed , float minSpeed , float maxSpeed ) {
+                if ( cruiseSpeed < minSpeed ) cruiseSpeed = minSpeed;
+                if ( cruiseSpeed > maxSpeed ) cruiseSpeed = maxSpeed;
+                this->cruiseSpeed = ( cruiseSpeed - minSpeed ) / ( maxSpeed - minSpeed );
+            }
+        } speedometerState;
+        struct {
+            uint8_t heading;
+            uint8_t range;
+            void setRange( float range , float minRange , float maxRange ) {
+                if ( range < minRange ) range = minRange;
+                if ( range > maxRange ) range = maxRange;
+                this->range = ( range - minRange ) / ( maxRange - minRange );
+            }
+        } sonarState;
+        void sendState();
     
     private:
         HardwareSerial *btSerial;
+        uint16_t sendCooldown;
         void waitForSerial();
+        struct {
+            uint8_t speed = 0;
+            uint8_t cruiseSpeed = 0;
+            bool cruiseControl = false;
+            bool lineFollowing = false;
+        } prevSpeedometerState;
+        struct {
+            uint8_t heading;
+            uint8_t range;
+        } prevSonarState;
+        uint32_t lastSendTime;
     
 };
 
